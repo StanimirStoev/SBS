27c86e4d8c4e1e575bf4aa8fd030b56289643b50
George Stoev <stoevgeorge@gmail.com>
2022-11-29 10:44:16 +0200

    Add extension to get UserId. Fix calulate with  ',' as decima ddelimiter.

---
+++ SBS/Extentions/ClaimsPrincipalExtension.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System.Security.Claims;
+
+namespace SBS.Extentions
+{
+    public static class ClaimsPrincipalExtension
+    {
+        public static string Id(this ClaimsPrincipal user)
+        {
+            return user.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+    }
+}
