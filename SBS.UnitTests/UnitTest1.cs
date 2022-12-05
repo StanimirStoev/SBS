35b1a36a20a506b3c70bd7f8d20924611c465883
George Stoev <stoevgeorge@gmail.com>
2022-12-05 16:51:12 +0200

    Added UnitTests project.

---
+++ SBS.UnitTests/UnitTest1.cs
@@ -0,0 +1,24 @@
+
+
+namespace SBS.UnitTests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+        [TearDown]
+        public void TearDown()
+        {
+
+        }
+    }
+}
